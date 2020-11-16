25d6def80f02ee4added7de7d8c5acf1c5ed5bd4
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-16 17:44:20 +0500

    Fix bug

--- js/utils.js
+++ js/utils.js
@@ -14,4 +14,6 @@ var createNewEl = function (tag, className, text) {
   var elNew = document.createElement(tag);
   elNew.setAttribute('class', className);
   elNew.textContent = text;
+
+  return elNew;
 }
