afd88203f65ea44259a3062b4f0cb5861405d301
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-16 17:39:53 +0500

    Minor imprivement

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,12 @@ var $_ = function (selector, node = document) {
 var $$_ = function (selector, node = document) {
   return node.querySelector(selector);
 }
+
+
+// Function to create new element
+var createNewEl = function (tag, className, text) {
+
+  var elNew = document.createElement(tag);
+  elNew.setAttribute('class', className);
+  elNew.textContent = text;
+}
